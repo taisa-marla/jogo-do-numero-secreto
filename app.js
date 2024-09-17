e7feb8eab74d2bcf31aa4b18951b4f20c46c0344
taisa-marla <taisa.marla.martins@gmail.com>
2024-09-17 01:50:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+let lista = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirMensagemInicial(){
+exibirTextoNaTela("h1", "Jogo do número secreto" );
+exibirTextoNaTela("p", "Escolha um numero entre 1 e 10");
+}
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto,"Brazilian Portuguese Female", {rate:1.2});
+}
+    exibirMensagemInicial();
+
+function verificarChute(){
+    let chute = document.querySelector ("input").value;
+
+    if (chute == numeroSecreto){
+        exibirTextoNaTela ("h1", "Acertou");
+        let palavraTentativa = tentativas > 1? "tentativas" : "tentativa";
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela ("p", mensagemTentativa);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    }else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela ("p", "o numero secreto é menor");
+        }else {
+            exibirTextoNaTela("p", "O numero secreto é maior");
+        }
+        tentativas++;
+
+        limparCampo();
+     
+    }
+}
+
+function gerarNumeroAleatorio (){
+   let numeroEscolhido =  parseInt(Math.random ()* numeroLimite + 1);
+   let quantidadesElemnetosLista = lista.length;
+
+   if (quantidadesElemnetosLista == numeroLimite){
+    lista = [];
+   }
+   if (lista.includes(numeroEscolhido)){
+    return gerarNumeroAleatorio();
+   }else{
+    lista.push(numeroEscolhido);
+    return numeroEscolhido;
+   }
+}
+
+function limparCampo(){
+    chute = document.querySelector("input");  
+    chute.value = " ";  
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById("reiniciar").setAttribute("disabled",true);
+}
